6262e666e671683fac249d55978731fe16e52a12
alex <alexlhliang@gmail.com>
2021-08-27 22:23:16 -0700

    finished tooltip

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,38 +8,18 @@ import './styles/HeaderStyles.css';
 const Header = (props) => {
   const [isHelp, setIsHelp] = useState(false);
 
-  const [loaded, setLoaded] = useState(false);
-  window.onresize = () => setLoaded((prev) => !prev);
-
-  const btnRef = useRef();
-
-  const btnCenter = useRef();
-
-  useEffect(function getBtnPos() {
-    const btnPos = btnRef.current.getBoundingClientRect();
-
-    btnCenter.current = findBtnCenter();
-
-    function findBtnCenter() {
-      const btnWidth = btnRef.current.offsetWidth;
-      const center = btnPos.left + btnWidth / 2;
-      return center;
-    }
-  });
-
   return (
     <div className="header">
-      <button
-        ref={btnRef}
-        type="button"
-        className="tooltip-btn"
-        onClick={() => setIsHelp((prev) => !prev)}
-      >
-        ?
-      </button>
-      {isHelp && (
-        <Tooltip btnCenter={btnCenter.current} close={() => setIsHelp(false)} />
-      )}
+      <div className="tooltip-ctn">
+        <button
+          type="button"
+          className="tooltip-btn"
+          onClick={() => setIsHelp((prev) => !prev)}
+        >
+          ?
+        </button>
+        {isHelp && <Tooltip close={() => setIsHelp(false)} />}
+      </div>
       <Scoreboard
         currentScore={props.currentScore}
         highScore={props.highScore}
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,48 +1,44 @@
 import React, { useRef, useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
 
 import './styles/TooltipStyles.css';
 
-const Tooltip = ({ btnCenter, close }) => {
-  console.log(btnCenter);
-  const [btnPos, setBtnPos] = useState();
-
+const Tooltip = ({ close }) => {
   const tooltipRef = useRef(null);
 
   const pxToShift = useRef(null);
 
   const [loaded, setLoaded] = useState(false);
 
-  useEffect(
-    function positionTooltip() {
-      console.log(btnCenter, 'hi');
-      if (!tooltipRef.current) return;
+  useEffect(function positionTooltip() {
+    console.log('hi');
+    window.addEventListener('click', close);
+    if (!tooltipRef.current) return;
 
-      const width = tooltipRef.current.offsetWidth;
-      const trianglePos = 15; // in %, the value of 'left' styling of tooltip:before aka the little triangle thing
+    const width = tooltipRef.current.offsetWidth;
+    const trianglePos = 15; // in %, the value of 'left' styling of tooltip:before aka the little triangle thing
+    const btnRadius = 15; // in px,
 
-      pxToShift.current = `${btnCenter - (width * trianglePos) / 100}px`;
-      setBtnPos(btnCenter);
-    },
-    [pxToShift, btnCenter]
-  );
+    pxToShift.current = `-${(width * trianglePos) / 100 - btnRadius}px`;
 
-  return ReactDOM.createPortal(
-    <div className="background" onClick={close}>
-      <div
-        className="tooltip"
-        ref={tooltipRef}
-        style={{
-          top: '73px',
-          left: pxToShift.current,
-        }}
-      >
-        How to play:
-        <br />
-        Don't click on the same card twice!
-      </div>
-    </div>,
-    document.getElementById('modal')
+    setLoaded(true);
+
+    return () => window.removeEventListener('click', close);
+  }, []);
+
+  return (
+    <div
+      className="tooltip"
+      ref={tooltipRef}
+      style={{
+        top: '43px',
+        left: pxToShift.current,
+      }}
+      onClick={(e) => e.stopPropagation()}
+    >
+      How to play:
+      <br />
+      Don't click on the same card twice!
+    </div>
   );
 };
 
